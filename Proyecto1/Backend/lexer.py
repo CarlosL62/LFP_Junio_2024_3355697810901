4b9c56af97cc785cb013900bfa16ef9776fe409c
CarlosL62 <carlosalfredolopez96@gmail.com>
2024-06-09 14:28:22 -0600

    Basic lexer

---
+++ Proyecto1/Backend/lexer.py
@@ -0,0 +1,108 @@
+class Lexer:
+    def __init__(self, textEntry):
+        self.textEntry = textEntry
+
+    def isValidCharacter(self, character):
+        return character in [';', '[', ']', ':', ',', '{', '}', '>']
+
+    def isWhitespace(self, character):
+        return character in [' ', '\t', '\n']
+
+    def analyze(self):
+        line = 1
+        column = 1
+        lexeme = ""
+
+        status = 0
+
+        for character in self.textEntry:
+            print(f'Procesando carácter: {character} ({line}, {column}), Estado: {status}')
+
+            if status == 0:
+                if character.isalpha():
+                    lexeme += character
+                    status = 1
+                    print(f'Estado 1: {lexeme}')
+                elif character == "-":
+                    lexeme += character
+                    status = 2
+                    print(f'Estado 2: {lexeme}')
+                elif character == "'":
+                    lexeme += character
+                    status = 3
+                    print(f'Estado 3: {lexeme}')
+                elif self.isValidCharacter(character):
+                    lexeme += character
+                    status = 4
+                    print(f'Estado 4: {lexeme}')
+                elif self.isWhitespace(character):
+                    if character == '\n':
+                        line += 1
+                        column = 0
+                    # Los espacios en blanco y saltos de línea son ignorados
+                else:
+                    print(f'ERROR LÉXICO: {character} ({line}, {column})')
+            elif status == 1:
+                if character.isalnum():
+                    lexeme += character
+                    print(f'Estado 1: {lexeme}')
+                else:
+                    print(f'Token IDENTIFICADOR: {lexeme} ({line}, {column})')
+                    print(f'ERROR LÉXICO: {character} ({line}, {column})')
+                    lexeme = ""
+                    status = 0
+                    continue
+            elif status == 2:
+                if character == ">":
+                    lexeme += character
+                    print(f'Token ARROW: {lexeme} ({line}, {column})')
+                    lexeme = ""
+                    status = 0
+                else:
+                    print(f'ERROR LÉXICO: {character} ({line}, {column})')
+                    lexeme = ""
+                    status = 0
+            elif status == 3:
+                if character == "'":
+                    lexeme += character
+                    print(f'Token STRING: {lexeme} ({line}, {column})')
+                    lexeme = ""
+                    status = 0
+                else:
+                    lexeme += character
+                    print(f'Estado 3: {lexeme}')
+            elif status == 4:
+                print(f'Token SIMBOLO: {lexeme} ({line}, {column})')
+                lexeme = ""
+                status = 0
+
+            # increase line and column
+            if character == '\n':
+                line += 1
+                column = 1
+            else:
+                column += 1
+
+        # ends the analysis
+        if status == 1:
+            print(f'Token IDENTIFICADOR: {lexeme} ({line}, {column})')
+        elif status == 3:
+            print(f'ERROR LÉXICO: Cadena sin cerrar ({line}, {column})')
+
+        print(f'Análisis completado. Último estado: {status}')
+
+
+# Ejemplo de uso
+text = '''nombre- -> 'Grafo Ejemplo'
+nodos -> [
+    'nodoA': 'Nodo 1',
+    'nodoB': 'Nodo 2',
+    'nodoC': 'Nodo 3'
+];
+conexiones -> [
+    'nodoA' > 'nodoB',
+    'nodoA' > 'nodoC'
+];
+'''
+lexer = Lexer(text)
+lexer.analyze()
--- Proyecto1/Frontend/mainWindow.py
+++ Proyecto1/Frontend/mainWindow.py
@@ -1,14 +1,32 @@
 import tkinter as tk
-from tkinter import ttk, filedialog, Tk, Text
+from tkinter import ttk, filedialog
+from Proyecto1.Backend import lexer
+
+# global variables
+file = None # file to be read
+
+fileToTry = '''nombre -> 'Grafos Guatemala'
+nodos -> [
+    'nodoA': 'Nodo 1',
+    'nodoB': 'Nodo 2',
+    'nodoC': 'Nodo 3'
+];
+conexiones -> [
+    'nodoA' > 'nodoB',
+    'nodoA' > 'nodoC'
+];'''
 
 
 def escribir():
     print("Hola, estoy escribiendo")
 
+
 def imprimir(txt):
     print(txt)
 
-def abrirArchivo():
+
+def openFile():
+    global file
     path = filedialog.askopenfilename(
         title= "Seleccione un archivo",
         initialdir= "C:/",
@@ -18,19 +36,28 @@ def abrirArchivo():
     )
 
     if path != "" and path is not None:
-        archivo = open(path, "r", encoding="utf-8")
-        print(archivo.read())
+        file = open(path, "r", encoding="utf-8")
+        print(file.read())
     else:
         print("No se seleccionó ningún archivo")
 
+
+def executeFile():
+    print("Ejecutando archivo")
+    global file
+    if fileToTry is not None:
+        lx = lexer.Lexer(fileToTry)
+        lx.analyze()
+    else:
+        print("No se seleccionó ningún archivo")
+
+
 # mainWindow
-main = Tk()
+main = tk.Tk()
 
 # main properties
 width_main = 1000
 heigth_main = 600
-
-#main.geometry(f"{ancho_ventana}x{alto_ventana}")
 
 # main title
 main.title("Grafos Guatemala")
@@ -48,18 +75,18 @@ main.geometry(f"{width_main}x{heigth_main}+{window_x}+{window_y}")
 lblFile = ttk.Label(main, text="Archivo")
 lblFile.place(x=25, y=50)
 
-btnAddFile = ttk.Button(main, text="Agregar archivo", command=abrirArchivo)
+btnAddFile = ttk.Button(main, text="Agregar archivo", command=openFile)
 btnAddFile.place(x=100, y=50)
-btnExecuteFile = ttk.Button(main, text="Ejecutar archivo", command=abrirArchivo)
+btnExecuteFile = ttk.Button(main, text="Ejecutar archivo", command=executeFile)
 btnExecuteFile.place(x=250, y=50)
 
 # reports
 lblReports = ttk.Label(main, text="Reportes")
 lblReports.place(x=25, y=100)
 
-btnTokensReport = ttk.Button(main, text="Reporte de tokens", command=abrirArchivo)
+btnTokensReport = ttk.Button(main, text="Reporte de tokens", command=openFile)
 btnTokensReport.place(x=100, y=100)
-btnErrorsReport = ttk.Button(main, text="Reporte de errores", command=abrirArchivo)
+btnErrorsReport = ttk.Button(main, text="Reporte de errores", command=openFile)
 btnErrorsReport.place(x=250, y=100)
 
 # images selection
@@ -70,7 +97,7 @@ combo.place(x=600, y=100)
 combo_values.append("Opción 4")
 
 # option for graph
-console = Text(main, width=110, height=25)
+console = tk.Text(main, width=110, height=25)
 console.place(x=50, y=150)
 
 #Mantener ventana abierta
