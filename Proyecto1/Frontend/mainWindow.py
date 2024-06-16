2bd5e3551349c6b75778caa1bd61ee193b1c5f8e
CarlosL62 <carlosalfredolopez96@gmail.com>
2024-06-16 03:43:43 -0600

    Save point, gif added, some errors solved when selecting a nongraph at the combo

--- Proyecto1/Frontend/mainWindow.py
+++ Proyecto1/Frontend/mainWindow.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageSequence
 from Backend.Lexer.lexer import Lexer
 from Backend.Reports.reportMaker import ReportMaker
 from Backend.Graphs.grapher import Grapher
@@ -11,7 +11,8 @@ lx = None  # lexer
 reportMaker = None  # report maker
 grapher = None  # grapher
 combo_values = []  # combobox values
-
+gif_frames = []  # frames of the GIF
+current_frame = 0  # current frame index
 
 def openFile():
     global file
@@ -44,7 +45,6 @@ def executeFile():
         lx.analyze()
         reportMaker = ReportMaker(lx.validTokens, lx.errorTokens)
 
-
         grapher = Grapher(lx.validTokens, lx.errorTokens)
         grapher.graphExtraxtor()
         if grapher.graphes.__len__() > 0:
@@ -53,6 +53,7 @@ def executeFile():
             messagebox.showinfo("Archivo ejecutado", "Archivo ejecutado y reportes generados")
         else:
             print("Archivo ejecutado, pero no se generaron grafos")
+            generateCombo([])
             messagebox.showwarning("Archivo ejecutado", "Archivo ejecutado, pero no se generaron grafos")
             if lx.errorTokens.__len__() > 0:
                 print("Errores léxicos encontrados")
@@ -67,14 +68,15 @@ def executeFile():
 def generateCombo(graphs):
     global combo, combo_values
     combo_values.clear()
-    for graph in graphs:
-        combo_values.append(graph.name)
-    print(combo_values)
-    combo.config(values=combo_values)
-    if combo_values:
-        combo.set(combo_values[0])
+    if graphs.__len__() > 0:
+        combo_values.append("Selecciona una imagen")
+        for graph in graphs:
+            combo_values.append(graph.name)
+        print(combo_values)
+        combo.config(values=combo_values)
     else:
-        combo.set("Selecciona una imagen")
+        combo_values.append("No hay imágenes disponibles")
+        combo.config(values=combo_values)
 
 
 def generateGraph(index):
@@ -82,8 +84,9 @@ def generateGraph(index):
     if grapher is not None:
         grapher.generateGraph(index)
         print(f"Grafo {index} generado")
-        messagebox.showinfo("Grafo generado", "Grafo generado")
-        updateImage(f'output/generatedGraph.png')
+        if index > -1:
+            messagebox.showinfo("Grafo generado", "Grafo generado")
+            updateImage(f'output/generatedGraph.png')
     else:
         print("No se ha ejecutado el análisis de archivo")
         messagebox.showerror("Error", "No se ha ejecutado el análisis de archivo")
@@ -132,7 +135,7 @@ def generateErrorsReport():
 def on_combobox_select(event):
     global combo
     selected_graph = combo.get()
-    graph_index = combo_values.index(selected_graph)
+    graph_index = combo_values.index(selected_graph) - 1
     generateGraph(graph_index)
 
 
@@ -140,13 +143,29 @@ def updateImage(image_path):
     global image_label
     try:
         img = Image.open(image_path)
-        img = img.resize((400, 400), Image.Resampling.LANCZOS)  # resize image
+        img = img.resize((500, 500), Image.Resampling.LANCZOS)  # resize image
         photo = ImageTk.PhotoImage(img)
         image_label.config(image=photo)
         image_label.image = photo
     except Exception as e:
         print(f"Error al cargar la imagen: {e}")
         messagebox.showerror("Error", f"No se pudo cargar la imagen: {e}")
+
+
+def load_gif(gif_path, size=(200, 200)):
+    global gif_frames, current_frame, gif_label
+    gif = Image.open(gif_path)
+    gif_frames = [ImageTk.PhotoImage(img.resize(size, Image.Resampling.LANCZOS)) for img in ImageSequence.Iterator(gif)]
+    current_frame = 0
+    animate_gif()
+
+
+def animate_gif():
+    global current_frame, gif_frames, gif_label
+    if gif_frames:
+        gif_label.config(image=gif_frames[current_frame])
+        current_frame = (current_frame + 1) % len(gif_frames)
+        main.after(100, animate_gif)
 
 
 # mainWindow
@@ -189,15 +208,19 @@ btnErrorsReport.place(x=250, y=100)
 # combo box for graph selection
 combo = ttk.Combobox(main, values=combo_values)
 combo.set("Selecciona una imagen")  # default value
-combo.place(x=600, y=100)
+combo.place(x=100, y=150, width=200)
 combo.bind("<<ComboboxSelected>>", on_combobox_select)
 
 # frame for image
 image_frame = ttk.Frame(main, width=110, height=25)
-image_frame.place(x=50, y=150)
-
+image_frame.place(x=450, y=50)
 image_label = ttk.Label(image_frame)
 image_label.pack()
+
+# Load and display the GIF
+gif_label = ttk.Label(main)
+gif_label.place(x=50, y=225)  # location of the GIF
+load_gif("resources/programmer.gif", size=(350, 350))  # Change the size as needed
 
 # keep window open
 main.mainloop()
