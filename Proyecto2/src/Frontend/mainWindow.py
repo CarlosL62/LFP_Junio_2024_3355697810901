cf2d17d2c267d055b119f7c09113e78905ee0db0
CarlosL62 <carlosalfredolopez96@gmail.com>
2024-06-20 23:01:30 -0600

    GUI improved, files management added and some test files added

--- Proyecto2/src/Frontend/mainWindow.py
+++ Proyecto2/src/Frontend/mainWindow.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog, messagebox
+from src.Backend.Files.file import File
 
 class MainWindow:
     def __init__(self):
@@ -15,6 +17,14 @@ class MainWindow:
         # Title
         self.label = tk.Label(self.window, text="Digitalizador de información", font=("Roboto", 24, "bold"), bg="lightgray", fg="black")
         self.label.pack()
+
+        # Separator
+        separator = tk.Frame(self.window, height=2, bd=1, relief=tk.SUNKEN)
+        separator.pack(fill=tk.X, padx=20, pady=10)
+
+        # File label
+        self.current_file_label = tk.Label(self.window, text="Archivo: ninguno", font=("Roboto", 10), bg="lightgray", fg="black")
+        self.current_file_label.pack(pady=10)
 
         # Menu
         menu_bar = tk.Menu(self.window)
@@ -37,7 +47,7 @@ class MainWindow:
 
         # Frame to contain the text editor
         self.text_editor_frame = tk.Frame(self.window, bg="white")
-        self.text_editor_frame.pack(pady=20, fill=tk.BOTH, expand=True)
+        self.text_editor_frame.pack(pady=20, padx=10, fill=tk.BOTH, expand=True)
 
         # Text editor
         self.text_editor = tk.Text(self.text_editor_frame, font=("Ubuntu Condensed", 16, "italic"), wrap=tk.WORD, height=12, width=40, bg="black", fg="white", insertbackground="white", insertwidth=2)
@@ -55,14 +65,42 @@ class MainWindow:
 
         self.window.mainloop()
 
+    selected_file = None # Variable to store the selected file
+
     def open_file(self):
-        pass
+        path = tk.filedialog.askopenfilename(
+            title="Seleccione un archivo",
+            filetypes=[
+                ("Archivos de entrada", "*.lfp")
+            ]
+        )
+        self.selected_file = None
+        self.selected_file = File(path)
+        self.selected_file.open_file()
+        # Update the label with the name of the selected file
+        self.current_file_label.config(text=f"Archivo: {self.selected_file.path}")
+        # Clear the text editor and insert the content of the selected file
+        self.text_editor.delete(1.0, tk.END)
+        self.text_editor.insert(1.0, self.selected_file.content)
 
     def save_file(self):
-        pass
+        if self.selected_file is not None:
+            self.selected_file.content = self.text_editor.get(1.0, tk.END)
+            self.selected_file.save_file()
+        else:
+            messagebox.showerror("Error", "No hay un archivo seleccionado")
 
     def save_as_file(self):
-        pass
+        file_to_save = File(None)
+        file_to_save.content = self.text_editor.get(1.0, tk.END)
+        save_path = tk.filedialog.asksaveasfilename(
+            title="Guardar archivo como",
+            defaultextension=".lfp",
+            filetypes=[
+                ("Archivos de entrada", "*.lfp")
+            ]
+        )
+        file_to_save.save_as_file(save_path)
 
     def generate_tokens_report(self):
         pass
